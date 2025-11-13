c80ebd3e85d44cb69d2863f18d94fa1fdf82bcc7
Ali <3lyly00@gmail.com>
2025-11-13 19:29:18 +0200

    Update queue_num.js

--- api/queue_num.js
+++ api/queue_num.js
@@ -1,11 +1,13 @@
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-
-  res.status(200).json({
-    queue_number: 50,
-    entry_time: Date.now(),
-    event_id: "WatsonsPH",
-    status: 1
-  });
+  res
+    .status(200)
+    .setHeader("Access-Control-Allow-Origin", "*")
+    .setHeader("Access-Control-Allow-Headers", "*")
+    .setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+    .json({
+      queue_number: 50,
+      entry_time: Date.now(),
+      event_id: "WatsonsPH",
+      status: 1
+    });
 }
